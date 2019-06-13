8690490e6051522589f92867f9fc132ab5f6ca32
RicardoCardona <ricardo.cardona@ksquareinc.com>
2019-06-13 16:19:17 -0500

    fix:fixed database problems with transactional and user

--- src/main/java/com/ksquareinc/chat/model/User.java
+++ src/main/java/com/ksquareinc/chat/model/User.java
@@ -17,8 +17,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-@Entity(name = "User")
-@Table(name = "User")
+@Entity(name = "Users")
+@Table(name = "Users")
 @Transactional
 public class User implements Serializable {
 	
