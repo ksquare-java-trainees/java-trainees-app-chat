221aca698e91baa67b5b8077b60e90b51c533805
judaramburoks <judith.aramburo@ksquareinc.com>
2019-06-14 10:48:49 -0500

    Fixing Security Config, making clear

--- src/main/java/com/ksquareinc/chat/config/AppInitializer.java
+++ src/main/java/com/ksquareinc/chat/config/AppInitializer.java
@@ -2,13 +2,11 @@ package com.ksquareinc.chat.config;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-import com.ksquareinc.chat.security.AuthTokenSecurityConfig;
-
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return new Class[] { AppConfig.class, AuthTokenSecurityConfig.class};
+		return new Class[] { AppConfig.class, SecurityConfig.class};
 	}
 
 	@Override
--- src/main/java/com/ksquareinc/chat/config/SecurityConfig.java
+++ src/main/java/com/ksquareinc/chat/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.ksquareinc.chat.security;
+package com.ksquareinc.chat.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -11,24 +11,30 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.access.ExceptionTranslationFilter;
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 
+import com.ksquareinc.chat.security.CustomAuthenticationManager;
+import com.ksquareinc.chat.security.PreAuthTokenHeaderFilter;
+
 @Configuration
 @EnableWebSecurity
 @PropertySource("classpath:application.properties")
 @Order(1)
-public class AuthTokenSecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
  
     @Value("${howtodoinjava.http.auth.tokenName}")
     private String authHeaderName;
  
+    /*@Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.build()
+    }*/
+    
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         
     	PreAuthTokenHeaderFilter filter = new PreAuthTokenHeaderFilter(authHeaderName);
         filter.setAuthenticationManager(new CustomAuthenticationManager());
          
-        httpSecurity.antMatcher("/api/**")
-            		.csrf()
-            			.disable()
+        httpSecurity.antMatcher("/api/**").csrf().disable()
             		.sessionManagement()
             			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             		.and()
