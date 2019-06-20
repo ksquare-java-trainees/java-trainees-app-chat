09fd4a24ec0e349a7e6b71c136a6d278e2c51b30
RicardoCardona <ricardo.cardona@ksquareinc.com>
2019-06-20 08:58:46 -0500

    feat:structure of error configuration

---
+++ src/main/java/com/ksquareinc/chat/controller/CustomRestExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.ksquareinc.chat.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomRestExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	
+}
--- src/main/java/com/ksquareinc/chat/error/ApiErrorResponse.java
+++ src/main/java/com/ksquareinc/chat/error/ApiErrorResponse.java
@@ -1,20 +1,32 @@
 package com.ksquareinc.chat.error;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
 
 @Data
 public class ApiErrorResponse {
 
 	private HttpStatus status;
+	
+	private String message;
+	
+	private List<String> errors;
+	
+	//Remove this one.
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
 	private LocalDateTime timestamp;
-	private String message;
-	private String detail;
-	//private List<ApiSubError> subErrors;
 	
+	//Remove this one.
+	private String detail;
+	
+	//Remove this one.
 	private ApiErrorResponse() {
 		timestamp = LocalDateTime.now();
 	}
@@ -31,11 +43,49 @@ public class ApiErrorResponse {
 		this.detail = ex.getLocalizedMessage();
 	}
 
-	public ApiErrorResponse(HttpStatus status, String message, Throwable ex) {
+	//Fix this one with HttpAuthenticationEntryPoint
+	/*public ApiErrorResponse(HttpStatus status, String message, Throwable ex) {
 		this();
 		this.status = status;
 		this.message = message;
 		this.detail = ex.getLocalizedMessage();
+	}*/
+	
+	public ApiErrorResponse(HttpStatus status, String message, String error) {
+		super();
+		this.status = status;
+		this.message = message;
+		this.errors = Arrays.asList(error);
 	}
 	
+	public ApiErrorResponse(HttpStatus status, String message, List<String> errors) {
+		super();
+		this.status = status;
+		this.message = message;
+		this.errors = errors;
+	}
+	
+	public HttpStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(HttpStatus status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public List<String> getErrors() {
+		return errors;
+	}
+
+	public void setErrors(List<String> errors) {
+		this.errors = errors;
+	}
 }
